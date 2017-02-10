fea57318ceeab505ac83650eb23f5a03c6b77b7b
kaveer <kaveer.rajcoomar@gmail.com>
2017-02-10 20:49:07 +0400

    Add onBackPress button method

--- app/src/main/java/com/kavsoftware/kaveer/splashscreen/MainActivity.java
+++ app/src/main/java/com/kavsoftware/kaveer/splashscreen/MainActivity.java
@@ -1,9 +1,11 @@
 package com.kavsoftware.kaveer.splashscreen;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,4 +18,18 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public void onBackPressed() {
+
+
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+
+        Toast.makeText(this, "Press the back button once again to close the application.", Toast.LENGTH_SHORT).show();
+    }
+
+
 }
